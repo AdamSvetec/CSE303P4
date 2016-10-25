0ed234dce7d803e22c86c8c56e0905b1765e5d5a
AdamSvetec <adamsvetec@gmail.com>
2016-10-25 10:20:56 -0400

    initial commit

---
+++ student.c
@@ -0,0 +1,9 @@
+#include "support.h"
+
+/*
+ * Please be sure to fill in this struct right away!
+ */
+struct student_t student = {
+  "Adam Svetec", /* first member name, e.g., Michael Spear */
+  "ajs217@lehigh.edu"  /* first member email, e.g., mfs409@lehigh.edu */
+};
